81a6ea3945b5e1279b39ac2c3dec38e0af5cedf2
Ali Elhefnawy <92871607+ElhefnawyDev@users.noreply.github.com>
2025-07-30 16:00:15 +0800

    Revert "last update salah hosting"
    This reverts commit 491eeb958bcce8b62525e6b43b212dfbb0b80778.

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -1,6 +1,4 @@
-// lib/cloudinary.ts
 import { v2 as cloudinary } from "cloudinary";
-import { UploadApiResponse } from "cloudinary";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
@@ -8,9 +6,4 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET!,
 });
 
-// ⬇️ أضف دالة الرفع
-export async function uploadToCloudinary(filePath: string): Promise<UploadApiResponse> {
-  return await cloudinary.uploader.upload(filePath, {
-    folder: "track", // يمكنك تغيير الاسم حسب مجلدك في Cloudinary
-  });
-}
+export default cloudinary;
